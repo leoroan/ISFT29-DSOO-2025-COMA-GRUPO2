ea31f356e2c278841008041a6e17f569ca301345
Leandro Maselli <leoroan@gmail.com>
2025-08-28 10:10:55 -0300

    Refactor clases Domicilio, Persona y Television para mejorar la legibilidad y consistencia del código

--- Semana3/A2/Domicilio.cs
+++ Semana3/A2/Domicilio.cs
@@ -6,40 +6,40 @@ using System.Threading.Tasks;
 
 namespace VisualizacionCompartida
 {
-    internal class Domicilio
+  internal class Domicilio
+  {
+    private string _calle;
+    private int _numero;
+    private string _barrio;
+
+    public Domicilio(string ca, int num, string ba)
     {
-        private string _calle;
-        private int _numero;
-        private string _barrio;
-
-        public Domicilio(string ca, int num, string ba)
-        {
-            this._calle = ca;
-            this._numero = num;
-            this._barrio = ba;
-        }
-
-        public string Calle
-        {
-            get { return _calle; }
-            set { _calle = value; }
-        }
-
-        public int Numero
-        {
-            get { return _numero; }
-            set { _numero = value; }
-        }
-
-        public string Barrio
-        {
-            get { return _barrio; }
-            set { _barrio = value; }
-        }
-
-        public override string ToString()
-        {
-            return $"Domicilio Calle:{_calle}, número:{_numero}, barrio:{_barrio}";
-        }
+      this._calle = ca;
+      this._numero = num;
+      this._barrio = ba;
     }
+
+    public string Calle
+    {
+      get { return _calle; }
+      set { _calle = value; }
+    }
+
+    public int Numero
+    {
+      get { return _numero; }
+      set { _numero = value; }
+    }
+
+    public string Barrio
+    {
+      get { return _barrio; }
+      set { _barrio = value; }
+    }
+
+    public override string ToString()
+    {
+      return $"Domicilio Calle:{_calle}, número:{_numero}, barrio:{_barrio}";
+    }
+  }
 }
