e3d8c8b058ef71aa9e2d619c4138237eff7cc901
Leandro Maselli <leoroan@gmail.com>
2025-08-28 08:35:42 -0300

    Add SuperHeroe class and Program for competition simulation
    - Implemented SuperHeroe class with properties for Fuerza, Resistencia, and SuperPoderes.
    - Added a Competir method to determine the outcome of a competition between two superheroes.
    - Created a Program class to instantiate superheroes and display competition results.

---
+++ Semana2/A2/SuperHeroe/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    internal static void Main(string[] args)
+    {
+        SuperHeroe superHeroe1 = new SuperHeroe("Batman", 90, 70, 0);
+        SuperHeroe superHeroe2 = new SuperHeroe("Superman", 95, 60, 70);
+        Console.WriteLine(superHeroe1.ToString());
+        Console.WriteLine(superHeroe2.ToString());
+        string resultado1 = superHeroe1.Competir(superHeroe2);
+        Console.WriteLine("1° " + resultado1); // Debería ser DERROTA
+        string resultado2 = superHeroe2.Competir(superHeroe1);
+        Console.WriteLine("2° " + resultado2); // Debería ser TRIUNFO
+    }
+}
