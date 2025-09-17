0edf7f25a0e1b31d033419b212be402d1db5794c
ProyectoIDE <proyecto.ide.arg@gmail.com>
2025-09-17 15:06:04 -0300

    17-09-25
    semana 4 A2.Prácticas formativas autogestionadas

---
+++ JAVIER-CANTEROS/4-Semana/A2.Prácticas formativas autogestionadas/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Empleado
+{
+    private string nombre;
+    private int edad;
+    private string cargo;
+
+    public Empleado(string nombre, int edad, string cargo)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cargo = cargo;
+    }
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Nombre: " + nombre + "," + "Edad: " + edad + "," + "Cargo: " + cargo);
+    }
+}
---
+++ JAVIER-CANTEROS/4-Semana/A2.Prácticas formativas autogestionadas/Test.cs
@@ -0,0 +1,16 @@
+﻿class Test
+{
+    static void Main()
+    {
+
+        Empresa empresa = new Empresa("Girosoft");
+
+        Empleado empleado = new Empleado("Javier Canteros", 43, "Desarrollador");
+
+        empresa.Empleado = empleado;
+
+
+        empresa.mostrarInformacion();
+    }
+}
+
