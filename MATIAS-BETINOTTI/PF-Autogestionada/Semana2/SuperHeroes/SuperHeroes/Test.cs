034e0faf23f862f9a8f9fed25fe39deceeb3b652
mbetinotti <mbetinotti@gmail.com>
2025-09-16 22:09:35 -0300

    practica no obligatoria. BETINOTTI

---
+++ MATIAS-BETINOTTI/PF-Autogestionada/Semana2/SuperHeroes/SuperHeroes/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Semana2;
+public class Test
+{
+    public static void Main()
+    {
+        SuperHeroe batman = new SuperHeroe("Batman", 90, 70, 0);
+        SuperHeroe superman = new SuperHeroe("Superman", 95, 60, 70);
+
+        string primeraBatalla = batman.competir(superman);
+        string segundaBatalla = superman.competir(batman);
+
+        Console.WriteLine("Resultado de Batman vs. Superman: " + primeraBatalla);
+        Console.WriteLine("Resultado de Superman vs. Batman: " + segundaBatalla);
+    }
+}
