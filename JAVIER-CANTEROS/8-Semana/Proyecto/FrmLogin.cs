9bd850ceae7b9095ab56051fbdb943c430397274
JAVIER CANTEROS <ZIROCOOL2003@HOTMAIL.COM>
2025-10-01 13:14:34 -0300

    01-10-25
    ejercicio semana 8

---
+++ JAVIER-CANTEROS/8-Semana/Proyecto/FrmLogin.cs
@@ -0,0 +1,27 @@
+using System.Data;
+
+namespace Proyecto;
+
+public partial class FrmLogin : Form
+{
+    public FrmLogin()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnIngresar_Click(object sender, EventArgs e)
+    {
+        DataTable tablaLogin = new DataTable(); // es la que recibe los datos desde el formulario
+        Datos.Usuarios dato = new Datos.Usuarios(); // variable que  contiene todas las caracteristicas de la clase
+        tablaLogin = dato.Log_Usu(txtUsuario.Text, txtPass.Text);
+        if (tablaLogin.Rows.Count > 0)
+        {
+            // quiere decir que el resultado tiene 1 fila por lo que el usuario EXISTE
+            MessageBox.Show("Ingreso exitoso");
+        }
+        else
+        {
+            MessageBox.Show("Usuario y/o password incorrecto");
+        }
+    }
+}
