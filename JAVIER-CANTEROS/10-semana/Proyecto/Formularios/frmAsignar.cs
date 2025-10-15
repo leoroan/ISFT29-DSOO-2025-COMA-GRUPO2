4362ddc51877859913c686c19c75c501935c324d
ProyectoIDE <ZIROCOOL2003@HOTMAIL.COM>
2025-10-15 00:31:10 -0300

    14-10-25
    comienzo del proyecto semana 10

---
+++ JAVIER-CANTEROS/10-semana/Proyecto/Formularios/frmAsignar.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using Proyecto.Datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto.Formularios
+{
+    public partial class frmAsignar : Form
+    {
+        public frmAsignar()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAsignar_Load(object sender, EventArgs e)
+        {
+            CargarGrilla();
+        }
+
+        private void CargarGrilla()
+        {
+            MySqlConnection sqlcon = new MySqlConnection();
+            try
+            {
+                string query;
+                sqlcon = Conexion.getInstancia().CrearConcexion();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
